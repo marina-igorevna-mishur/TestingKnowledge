18da39b0d01a2f90196020bc0aaf7e926ed10e28
plantysnake <plantysnake@gmail.com>
2020-02-28 13:34:04 +0300

    Built bundle with webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
+import './style.css';
+
 function hello () {
   const element = document.createElement('div');
         
   element.innerHTML = "Hello World!";
   element.classList.add("hello");
-  element.style.color = "green";
-  element.style.fontSize = "52px";
-  element.style.fontWeight = 'lighter';
-  element.style.textAlign = 'center';
   return element;
 }
 
