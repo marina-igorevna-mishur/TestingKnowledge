b2cd10be8dfb3a9ac83d5b27d01141821cf684c6
plantysnake <plantysnake@gmail.com>
2020-02-28 14:36:14 +0300

    Added jsx support

---
+++ src/js/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../css/style.css';
+
+export default class App extends React.Component {
+    render() {
+        const element = <div className="hello"> Hello World! </div>;
+        return(
+            element
+        );
+    }
+}
