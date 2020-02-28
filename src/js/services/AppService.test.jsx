e8e24ca798be76de634b6ba287dc6a7e8652055d
plantysnake <plantysnake@gmail.com>
2020-02-28 15:18:09 +0300

    Added first test with jest

---
+++ src/js/services/AppService.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { getData } from './AppService.jsx';
+
+describe('testing primitive', () => {
+    it('returns hello world"?"', () => {
+        expect(getData()).toBe("Hello World?");
+    });
+});
