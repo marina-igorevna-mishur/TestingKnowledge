e80433cbcd086b401721fd289ceff045353578a6
plantysnake <plantysnake@gmail.com>
2020-03-04 14:33:03 +0300

    Fixed code style and added unmount of the component in tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import App from 'components/App.jsx';
-import AppService from 'services/AppService.jsx';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react"
+import Enzyme, { shallow } from "enzyme"
+import App from "components/App.jsx"
+import AppService from "services/AppService.jsx"
+import Adapter from "enzyme-adapter-react-16"
 
-const sinon = require('sinon');
+const sinon = require("sinon")
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('App component', () => {
-    it('gets right string', () => {
-      const wrapper = shallow(<App />);
-      const text = wrapper.find('div').text();
-      expect(text).toEqual('Hello World?');
-    });
+describe("App component", () => {
+	var wrapper
 
-    it('gets not string', () => {
-      sinon.stub(AppService, 'getData').returns('What?');
-      const wrapper = shallow(<App />);
-      const text = wrapper.find('div').text();
-      expect(text).toEqual('What?');
-    });
-  });
+	beforeEach(() => {
+		wrapper = shallow(<App />)
+	})
+
+	afterEach(() => {
+		wrapper.unmount()
+	})
+
+	it("gets right string", () => {
+		var text = wrapper.find("div").text()
+		expect(text).toEqual("Hello World?")
+	})
+
+	it("gets not right string", () => {
+		var wrong = sinon.stub(AppService, "getData").returns("What?")
+		var text = wrapper.find("div").text()
+		expect(text).toEqual("What?")
+		wrong.restore()
+	})
+})
--- tests/AppService.test.jsx
+++ tests/AppService.test.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { getData } from 'services/AppService.jsx';
+import React from "react"
+import ReactDOM from "react-dom"
+import { getData } from "services/AppService.jsx"
 
-describe('testing primitive', () => {
-    it('returns hello world"?"', () => {
-        expect(getData()).toBe("Hello World?");
-    });
-});
+describe("testing primitive", () => {
+	it("returns hello world\"?\"", () => {
+		expect(getData()).toBe("Hello World?")
+	})
+})
